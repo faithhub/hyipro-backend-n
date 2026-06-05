648e0d0903800cd86e24b3bcfeeb6bd360b7b6d5
faithhub <adebayooluwadara@gmail.com>
2026-06-05 22:17:03 +0100

    Update from CORS

--- src/app.js
+++ src/app.js
@@ -44,7 +44,8 @@ app.use(morgan('combined'));
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173',
-  'http://hyipro.online',
+  'https://hyipro.online',
+  'https://www.hyipro.online',
   'https://hyipro-frontend.pages.dev',
   'http://127.0.0.1:5173',
   process.env.FRONTEND_URL
